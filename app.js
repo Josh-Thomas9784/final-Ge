4cc5db5a5a4d5882ea9621e59d850ef4f3c67646
Josh-Thomas9784 <114836831+Josh-Thomas9784@users.noreply.github.com>
2024-03-17 15:16:47 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      }
+  })
+})
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const observer1 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show2');
+      } else{
+        entry.target.classList.remove('show2');
+      }
+  })
+})
+
+
+const hiddenElements1 = document.querySelectorAll('.hidden2');
+hiddenElements1.forEach((el) => observer1.observe(el));
+
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show3');
+      } else{
+        entry.target.classList.remove('show3');
+      }
+  })
+})
+
+
+const hiddenElements2 = document.querySelectorAll('.hidden3');
+hiddenElements2.forEach((el) => observer2.observe(el));
